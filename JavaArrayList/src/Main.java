bef3906f23294483aa4b884dd36c29c1d2d0cd87
MykolaDekusar <ndekusar97@gmail.com>
2024-10-29 12:42:07 +0100

    Use of .size(), .remove(), .clear(), .get(), .set() on ArrayList

--- JavaArrayList/src/Main.java
+++ JavaArrayList/src/Main.java
@@ -18,6 +18,25 @@ public class Main {
 		prezzi.add(10);
 		prezzi.add(15);
 		prezzi.add(8);
+		
+		cibi.set(0, "Ramen"); //modifichiamo il dato in posizione 0 da pasta a Ramen
+		//prendiamo l'elemento in posizione 0 e lo stampiamo
+		//stampiamo tutti gli elemti dell'ArrayList ciclando sulla lunghezza usando .size
+		for(int i=0;i<cibi.size();i++) {
+			System.out.print(cibi.get(i)+" ");
+			System.out.print(prezzi.get(i)+"€");
+			System.out.println();
+		}
+		System.out.println();
+		//rimuoviamo l'elemento in posizione index 2
+		prezzi.remove(2);
+		cibi.remove(2);
+		for(int i=0;i<cibi.size();i++) {
+			System.out.print(cibi.get(i)+" ");
+			System.out.print(prezzi.get(i)+"€");
+			System.out.println();
+		}
+		cibi.clear(); //svuotiamo completamente l'ArrayList cibi
 	}
 
 }
